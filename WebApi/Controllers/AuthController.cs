6d166afd435a4c85ab1d897026f4ac97c31680e0
The Hien <phamthehienc20042000@gmail.com>
2022-10-06 09:52:44 +0700

    b1:Create JSON Web Tokens

---
+++ WebApi/Controllers/AuthController.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using NuGet.Common;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public static User user = new User();
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto userDto)
+        {
+            user.Username = userDto.Username;
+            user.Password = userDto.Password;
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto userDto)
+        {
+            if (userDto.Username != user.Username)
+            {
+                return BadRequest("tai khoan khong ton tai");
+            }
+
+            if (!VerifyPassword(userDto))
+            {
+                return BadRequest("sai mat khau");
+            }
+
+            string token = CreateToken(user);
+            return token;
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+            var key = new SymmetricSecurityKey(
+                System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials:cred
+            );
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+        }
+        private bool VerifyPassword(UserDto userDto)
+        {
+            if (userDto.Password != user.Password)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
+    }
+}
